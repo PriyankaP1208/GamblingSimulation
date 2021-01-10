3d08698e97c1ea29c2140bcef3721df9f3221cbd
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-01-10 20:41:17 +0530

    Added:Won maximum and Lost minimum

--- utility.js
+++ utility.js
@@ -9,6 +9,8 @@ class GamblingSimulation
         let days=0;
         let totalWin=0;
         let totalLoss=0;
+        var array1=[];
+        var array2=[];
         while(days<20)
         {
             let luck=Math.floor(Math.random()*2)+1;
@@ -18,7 +20,8 @@ class GamblingSimulation
                 money=money+i;
                 totalWin=totalWin+(money/2);
                 money=money+(money/2);
-                console.log("Won","day",days,"By->",money);
+                array1.push(money);
+                //console.log("win",array1);
             }
             else
             {
@@ -26,12 +29,30 @@ class GamblingSimulation
                 money=money-i;
                 totalLoss=totalLoss+(money/2);
                 money=money/2;
-                console.log("Lost","day",days,"By->",money);
+                array2.push(money);
+                //console.log("Lost",array2);
             }
             days++;
         }
+        console.log(array1);
+        console.log(array2);
+        let luckiest=array1[0];
         console.log("Total win:",totalWin);
         console.log("Total loss:",totalLoss);
+        for (let i = 1; i < array1.length; i++) {
+            if (array1[i] > luckiest) {
+              luckiest = array1[i];
+            }
+        }
+        console.log(luckiest);
+        let unlukiest = array2[0];
+        for (let i = 1; i < array2.length; ++i) {
+            if (array2[i] < unlukiest) {
+                unlukiest = array2[i];
+            }
+        }
+        console.log(unlukiest);
+        
     }
 }
 module.exports=new GamblingSimulation();
