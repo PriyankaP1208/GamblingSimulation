085ffeb20a67954f8663167050a9a13060c23263
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-01-10 11:46:41 +0530

    Added:Gabler will either win or loose

--- utility.js
+++ utility.js
@@ -4,6 +4,15 @@ class GamblingSimulation
    {
        const STAKE=100;
        const BET=1;
+       let bet=Math.floor(Math.random()*2)+1;
+       if(bet==1)
+       {
+           console.log("Win");
+       }
+       else
+       {
+           console.log("Loose")
+       }
    }
 }
 module.exports=new GamblingSimulation();
