7fbbbc55a90b0b370dd31f061a4f32ebfac25a8b
PriyankaP1208 <ppriyapatil1208@gmail.com>
2021-01-10 11:57:34 +0530

    Added:Gambler if won or loss 50% of stake would resign for a day

--- utility.js
+++ utility.js
@@ -4,14 +4,20 @@ class GamblingSimulation
    {
        const STAKE=100;
        const BET=1;
-       let bet=Math.floor(Math.random()*2)+1;
-       if(bet==1)
+       let money=STAKE;
+       let i=BET;
+       let luck=Math.floor(Math.random()*2)+1;
+       if(luck==1)
        {
            console.log("Win");
+           money=money+i;
+           money=money/2;
        }
        else
        {
            console.log("Loose")
+           money=money-i;
+           money=money/2;
        }
    }
 }
